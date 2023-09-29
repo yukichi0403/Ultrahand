0c5a66b108ee11726bcf983a3cf96589dce3c08b
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-09-30 00:59:46 +0900

    Update skewness.py

--- skewness.py
+++ skewness.py
@@ -5,6 +5,8 @@ numeric_cols = df_train.select_dtypes(include = np.number).columns.to_list()
 skew_features = features[numeric_cols].apply(lambda x: skew(x)).sort_values(ascending=False)
 
 # NOTE: `skewness`が`0.5`を超える列だけに絞り込む
-high_skew = skew_features[skew_features > 0.5]
+high_skew = skew_features[skew_features > 0.75]
 skew_index = high_skew.index
 
+# log1p関数で正規分布に近い形に変換
+df_train[skew_index] = np.log1p(df_train[skew_index])
