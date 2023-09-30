ebaad1414a9e30d740f97f580cd1d42c2fccd837
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-10-01 00:46:37 +0900

    Update and rename skewness.py to skewness engineering.py

---
+++ skewness engineering.py
@@ -0,0 +1,16 @@
+from scipy.stats import skew
+
+skewed_feats = all_data[numeric_cols].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+print("\nSkew in numerical features: \n")
+skewness = pd.DataFrame({'Skew' :skewed_feats})
+skewness.head(15)
+
+skewness = skewness[abs(skewness) > 0.75]
+print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
+
+from scipy.special import boxcox1p
+skewed_features = skewness.index
+lam = 0.15
+for feat in skewed_features:
+    #all_data[feat] += 1
+    all_data[feat] = boxcox1p(all_data[feat], lam)
--- skewness.py
+++
@@ -1,4 +0,0 @@
-skewed_feats = all_data[numeric_cols].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
-print("\nSkew in numerical features: \n")
-skewness = pd.DataFrame({'Skew' :skewed_feats})
-skewness.head(15)
