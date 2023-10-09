a508b6295d0767c116d3e511a806dd0398fdac8b
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-10-09 20:10:40 +0900

    Update imports.py

--- 基本のコード/imports.py
+++ 基本のコード/imports.py
@@ -9,7 +9,7 @@ import datetime                # Datetime for date and time operations
 from sklearn.model_selection import train_test_split, StratifiedKFold, KFold, RepeatedKFold, cross_val_score  # Cross-validation techniques
 from sklearn.metrics import roc_auc_score, accuracy_score, log_loss  # Evaluation metrics
 from sklearn.model_selection import cross_validate  # Cross-validation scoring
-from sklearn.metrics import RocCurveDisplay, confusion_matrix, ConfusionMatrixDisplay, precision_score, average_precision_score, mean_squared_error # Metrics and displays
+from sklearn.metrics import roc_curve, confusion_matrix, ConfusionMatrixDisplay, precision_score, average_precision_score, auc, f1_score, mean_squared_error # Metrics and displays
 from sklearn.preprocessing import StandardScaler, RobustScaler
 from sklearn.linear_model import Lasso, Ridge, LogisticRegression  # Regression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier  # Random Forest and Gradient Boosting
