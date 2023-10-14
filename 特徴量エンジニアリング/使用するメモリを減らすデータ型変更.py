ea5250ade7fe47b583800c29fc15271753283c51
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-10-15 00:34:10 +0900

    Create 使用するメモリを減らすデータ型変更.py

---
+++ 特徴量エンジニアリング/使用するメモリを減らすデータ型変更.py
@@ -0,0 +1,26 @@
+def reduce_mem_usage(df):
+    
+    """ 
+    Iterate through all numeric columns of a dataframe and modify the data type
+    to reduce memory usage.        
+    """
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object and not is_datetime64_ns_dtype(df[col]) and not 'category':
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int32)  
+            else:
+                df[col] = df[col].astype(np.float16)
+        
+    return df
