c813cf4ad6d21409042af7d30f1a25902415ea10
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-10-20 21:28:35 +0900

    Update 使用するメモリを減らすデータ型変更.py

--- 特徴量エンジニアリング/使用するメモリを減らすデータ型変更.py
+++ 特徴量エンジニアリング/使用するメモリを減らすデータ型変更.py
@@ -1,26 +1,38 @@
-def reduce_mem_usage(df):
+# Reduce Memory Usage
+# reference : https://www.kaggle.com/code/arjanso/reducing-dataframe-memory-size-by-65 @ARJANGROEN
+
+def reduce_memory_usage(df):
     
-    """ 
-    Iterate through all numeric columns of a dataframe and modify the data type
-    to reduce memory usage.        
-    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
     
     for col in df.columns:
-        col_type = df[col].dtype
+        col_type = df[col].dtype.name
+        if ((col_type != 'datetime64[ns]') & (col_type != 'category')):
+            if (col_type != 'object'):
+                c_min = df[col].min()
+                c_max = df[col].max()
 
-        if col_type != object and not is_datetime64_ns_dtype(df[col]) and not 'category':
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int32)  
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float16)
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        pass
             else:
-                df[col] = df[col].astype(np.float16)
-        
+                df[col] = df[col].astype('category')
+    mem_usg = df.memory_usage().sum() / 1024**2 
+    print("Memory usage became: ",mem_usg," MB")
+    
     return df
