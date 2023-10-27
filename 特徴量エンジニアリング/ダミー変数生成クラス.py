b9acc7e1df4308271f27dccfe865f6143ddfe3e8
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-10-27 23:08:25 +0900

    Update ダミー変数生成クラス.py

--- 特徴量エンジニアリング/ダミー変数生成クラス.py
+++ 特徴量エンジニアリング/ダミー変数生成クラス.py
@@ -1,14 +1,14 @@
+from sklearn.base import BaseEstimator, TransformerMixin
 #ダミー変数生成クラスを自作
-from sklearn.base import BaseEstimator,TransformerMixin
-class Get_Dummies(BaseEstimator,TransformerMixin):
+class GetDummies(BaseEstimator, TransformerMixin):
+    
     def __init__(self):
         self.columns = None
-    
-    def fit(self,X,y=None):
+        
+    def fit(self, X, y=None):
         self.columns = pd.get_dummies(X).columns
         return self
     
-    def transform(self,X):
+    def transform(self, X):
         X_new = pd.get_dummies(X)
-        X_new.reindex(self.columns,fill_value=0)
-        return X_new
+        return X_new.reindex(columns=self.columns, fill_value=0)
